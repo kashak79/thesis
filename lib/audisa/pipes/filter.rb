a14dbf270b604eb07ddcd8ec145f725ec6c0affd
Mattias Putman <mattias.putman@gmail.com>
2011-07-12 20:38:48 +0200

    better filters

--- lib/audisa/pipes/filter.rb
+++ lib/audisa/pipes/filter.rb
@@ -6,7 +6,9 @@ class Pipes::Filter < Core::Pipe
   end
 
   def execute(*args)
-    push(*args) if @filter.call(*args)
+    filtered = @filter.call(*args)
+    filtered_data = filtered.kind_of?(Array) ? filtered : [filtered]
+    push(*filtered_data) if filtered
   end
 
 end
