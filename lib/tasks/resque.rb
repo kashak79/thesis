00ecba86dc1dfbc7ad329ea927d4c7676a48ebcf
Mattias Putman <mattias.putman@gmail.com>
2011-07-12 11:07:23 +0200

    getting output from async execution

--- lib/tasks/resque.rb
+++ lib/tasks/resque.rb
@@ -4,7 +4,7 @@ namespace :resque do
   desc 'Start the BJ software with one worker'
   task :start do
     sh %{resque-web -p 8080 > /dev/null 2>&1 &}
-    sh %{QUEUE=* rake resque:work > /dev/null &}
+    sh %{QUEUE=* rake resque:work &}
   end
 
   desc 'Stop the BJ software'
