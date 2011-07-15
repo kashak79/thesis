7410bd35d62266d9f5f249d2a05f103a3e5f751f
Simon Buelens <simonbuelens@gmail.com>
2011-07-15 19:04:19 +0200

    whitespace.rb terug goed gezet

--- lib/tasks/whitespace.rb
+++ lib/tasks/whitespace.rb
@@ -1,15 +1,14 @@
 namespace :whitespace do
   desc 'Removes trailing whitespace'
   task :cleanup do
-    sh %{for f in `find lib testpipe.rb config -type f | grep 
--v -e '.git/' -e 'public/' -e '.png'`;
+    sh %{for f in `find lib testpipe.rb config -type f | grep -v -e '.git/' -e 'public/' -e '.png'`;
           do cat $f | sed 's/[ \t]*[\r]*$//' > tmp; cp tmp $f; rm tmp; echo -n .;
         done}
   end
 
   desc 'Converts hard-tabs into two-space soft-tabs'
   task :retab do
-    sh %{for f in `find lib -type f | grep -v -e '.git/' -e 'public/' -e '.png' -e '.jar'`;
+    sh %{for f in `find lib -type f | grep -v -e '.git/' -e 'public/' -e '.png'`;
           do cat $f | sed 's/\t/  /g' > tmp; cp tmp $f; rm tmp; echo -n .;
         done}
   end
